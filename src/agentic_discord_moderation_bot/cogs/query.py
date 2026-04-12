35fd42372ecdc9109da8e633435eef6e3fbf2ee4
Aramythia <54689554+Aramythia@users.noreply.github.com>
2026-04-12 12:21:43 -0400

    Use an LCEL chain in query command

--- src/agentic_discord_moderation_bot/cogs/query.py
+++ src/agentic_discord_moderation_bot/cogs/query.py
@@ -1,6 +1,9 @@
 import discord
 from discord.ext import commands
-from langchain_core.messages import SystemMessage, HumanMessage, AIMessage
+
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate
+
 from agentic_discord_moderation_bot.utils.AgentBot import AgentBot
 
 
@@ -8,14 +11,18 @@ class Query(commands.Cog):
     def __init__(self, bot: AgentBot):
         self.bot = bot
 
+        prompt = ChatPromptTemplate.from_messages([
+            ("system", "You are a helpful assistant. Limit your response to at most 30 words."),
+            ("human", "{question}")
+        ])
+
+        self.query_chain = prompt | self.bot.ai.llm | StrOutputParser()
+
     @commands.slash_command(description="Query the bot with a question")
     @discord.option("question", description="The question you want to ask the bot")
     async def query(self, ctx: discord.ApplicationContext, *, question: str):
-        response = await self.bot.ai.llm.ainvoke([
-            SystemMessage(content="You are a helpful assistant. Limit your response to at most 30 words."),
-            HumanMessage(content=question)
-        ])
-        await ctx.respond(response.content)
+        response = await self.query_chain.ainvoke({"question": question})
+        await ctx.respond(response)
 
 
 def setup(bot: discord.Bot):
