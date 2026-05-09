d53e74dbb353500c7336581235d23869dd00f9a4
Aramythia <54689554+Aramythia@users.noreply.github.com>
2026-05-09 16:44:46 -0400

    Invoke graph on_message

--- src/agentic_discord_moderation_bot/cogs/reader.py
+++ src/agentic_discord_moderation_bot/cogs/reader.py
@@ -2,11 +2,13 @@ import discord
 from discord.ext import commands
 
 from agentic_discord_moderation_bot.utils.AgentBot import AgentBot
+from agentic_discord_moderation_bot.utils.graph import create_graph
 
 
 class Reader(commands.Cog):
     def __init__(self, bot: AgentBot):
         self.bot = bot
+        self.graph = create_graph(bot.ai.llm)
 
     @commands.Cog.listener()
     async def on_message(self, message: discord.Message):
@@ -14,6 +16,9 @@ class Reader(commands.Cog):
             return
         
         print(f"Message from {message.author}: {message.content}")
+        result = await self.graph.ainvoke({"message_ctx": message})
+        print(result)
+        await message.reply(result["response"])
         
 
 def setup(bot: discord.Bot):
